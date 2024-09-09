7b7c39a6f9ff2aa8beb8972770448d9e19ec3e63
Macedo Francisco <macedolb53@gmail.com>
2024-09-09 23:25:49 +0200

    feat(banner): add confirmado

--- barber-app/app/page.tsx
+++ barber-app/app/page.tsx
@@ -4,6 +4,9 @@ import Header from "./_components/Header";
 import { Button } from "@/components/ui/button";
 import { SearchIcon } from "lucide-react";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 
 
@@ -33,6 +36,36 @@ const Home = () => {
           <div className="relative w-full h-[150px] mt-6">
             <Image src="/Banner.png" fill  className="object-cover rounded-xl"/>
           </div>
+
+
+           {/*Agendamento*/}
+           <Card className="mt-6">
+            <CardContent className="flex justify-between p-0">
+              <div className=" flex flex-col gap-2 py-5 pl-5">
+
+                <Badge className="w-fit">Confirmado</Badge>
+
+                <h3 className="font-semibold">Corte de Cabelo</h3>
+
+                <div className="flex items-center gap-2">
+                  <Avatar className="h-6 w-6">
+                   <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
+                  </Avatar>
+                  <p className="text-sm">
+                    Barbearia Lumi
+                  </p>
+                </div>
+
+              </div>
+
+                <div className="flex flex-col justify-center items-center border-l-2 border-solid px-5">
+                  <p className="text-sm">Dezembro</p>
+                  <p className="text-3xl">12</p>
+                  <p className="text-sm">20:00</p>
+
+                </div>
+            </CardContent>
+           </Card>
 
         
       </div>
