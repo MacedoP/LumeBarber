f2f0afdb642c60864d3802d0d4ee587dca011cc3
Macedo Francisco <macedolb53@gmail.com>
2024-09-09 21:47:21 +0200

    chore(shadcn): add instaltion

--- barber-app/app/page.tsx
+++ barber-app/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return ( 
     <div>
       <h1 className="text-green-500 text-center">Hello React-next</h1>
-     
+     <Button>Click</Button>
     </div>
    );
 }
