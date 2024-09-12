f3d66a5c0a9e170f105c7ab511f84f8bae78b632
Macedo Francisco <macedolb53@gmail.com>
2024-09-12 11:12:30 +0200

    feat(barber): add barbershop components

--- barber-app/app/_components/barbershop-item.tsx
+++ barber-app/app/_components/barbershop-item.tsx
@@ -1,6 +1,9 @@
 import { Barbershop } from "@prisma/client";
 import { Card, CardContent } from "./ui/card";
 import  Image  from "next/image";
+import { Button } from "@/components/ui/button";
+import { Badge} from "@/components/ui/badge";
+import { StarIcon } from "lucide-react";
 
 interface BarbershopItemProps {
     barbershop: Barbershop
@@ -9,13 +12,29 @@ interface BarbershopItemProps {
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
     return ( 
-        <Card className="min-w-[159px]">
+        <Card className="min-w-[167px] rounded-2xl">
             {/*Imagens das barbearias*/}
-            <CardContent className="p-0">
-                <div className="relative h-[159px] w-full">
-                   <Image fill className="object-cover" src={barbershop.imageUrl}  alt="Barber Imagens"/>
+            <CardContent className=" p-2">
+                <div className="relative h-[159px] w-full container_img">
+                   <Image fill className="object-cover rounded-2xl barber_img" src={barbershop.imageUrl}  alt="Barber Imagens"/>
+
+                   <Badge className="absolute  left-2 top-2 z-50" variant="secondary">
+                        <StarIcon size={12} className="fill-primary text-primary"/>
+                        <p className="ml-1 font-semibold text-xs">5,0</p>
+                   </Badge>
 
                 </div>
+
+                {/*TEXTOS ABAIXO DAS NOSSAS IMAGENS*/}
+                <div className=" py-3">
+                    <h3 className="font-semibold overflow-hidden text-nowrap text-ellipsis truncate">{barbershop.name}</h3>
+                    <p className="text-sm text-gray-400 overflow-hidden text-ellipsis truncate">{barbershop.address}</p>
+                    <Button className="w-full mt-3" variant="secondary">
+                        Reservar
+                    </Button>
+
+                </div>
+
             </CardContent>
         </Card>
      );
